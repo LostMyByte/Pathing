aaa9b2e40658993fdbc121380c7eb3afd17768a2
Kieran <lostmybyte@gmail.com>
2025-09-07 12:31:22 -0400

    Path Builder, needs testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/Testing/MPCTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAOpModes/Testing/MPCTest.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.AAAOpModes.Testing;
 
-import static org.firstinspires.ftc.teamcode.AAAOpModes.Testing.MPCTest.MPCParams.*;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,11 +7,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.AAAOpModes.BaseOpMode;
 import org.firstinspires.ftc.teamcode.Motion.Controllers.ConstantSignal;
 import org.firstinspires.ftc.teamcode.Motion.Controllers.MPC;
+import org.firstinspires.ftc.teamcode.Motion.Controllers.MPCPath;
 import org.firstinspires.ftc.teamcode.Motion.Controllers.Signal;
 import org.firstinspires.ftc.teamcode.Motion.DriveModel;
 import org.firstinspires.ftc.teamcode.Motion.Drivetrains.FixedDriveTrain;
 import org.firstinspires.ftc.teamcode.Motion.Localization.Location;
 import org.firstinspires.ftc.teamcode.Motion.Movement;
+import org.firstinspires.ftc.teamcode.Utilities.Configuration.DriveConfig;
 import org.firstinspires.ftc.teamcode.Utilities.Math.GeneralMatrix;
 import org.firstinspires.ftc.teamcode.Utilities.Math.Matrix;
 import org.firstinspires.ftc.teamcode.Utilities.Math.Vector;
@@ -23,60 +23,21 @@ import org.opencv.core.Mat;
 public class MPCTest extends BaseOpMode {
 
     @Config
-    public static class MPCParams {
-        public static double QX = 100;
-        public static double QY = 100;
-        public static double QH = 100;
-        public static double QHV = 10;
-        public static double QXV = 10;
-        public static double QYV = 10;
-
-        public static double QFX = 20;
-        public static double QFY = 20;
-        public static double QFH = 20;
-        public static double QFHV = 10;
-        public static double QFXV = 10;
-        public static double QFYV = 10;
-
-        public static double R = 50;
+    public static class TestMPCParams {
 
         public static int N = 40;
         public static int Horizon = 5;
         public static double threshold = 0.0001;
-        public static double lr = 1.01;
-        public static double max_lambda = 1000;
-        public static double max_vxx = 1000000;
 
         public static double TX = 0;
         public static double TY = 10;
         public static double TH = 0;
-        public static double TXV = 0;
-        public static double TYV = 0;
+        public static double TV = 0;
         public static double THV = 0;
 
     }
 
-    private Matrix Q = new GeneralMatrix(6, 6, new double[] {
-            QX, 0, 0, 0, 0, 0,
-            0, QY, 0, 0, 0, 0,
-            0, 0, QH, 0, 0, 0,
-            0, 0, 0, QXV, 0, 0,
-            0, 0, 0, 0, QYV, 0,
-            0, 0, 0, 0, 0, QHV,
-    });
-
-    private Matrix QF = new GeneralMatrix(6, 6, new double[] {
-            QFX, 0, 0, 0, 0, 0,
-            0, QFY, 0, 0, 0, 0,
-            0, 0, QFH, 0, 0, 0,
-            0, 0, 0, QFXV, 0, 0,
-            0, 0, 0, 0, QFYV, 0,
-            0, 0, 0, 0, 0, QFHV,
-    });
-
-    private Matrix RM = Matrix.identityMatrix(3).multiplied(R);
-
-    private MPC test;
+    private MPCPath test;
     private Movement drive;
 
     @Override
@@ -85,7 +46,20 @@ public class MPCTest extends BaseOpMode {
         DriveModel.reInit();
 
         drive = new FixedDriveTrain(new Vector(0, 0, 0));
-        test = new MPC(new ConstantSignal(new Vector(new double[] {TX, TY, TH, TXV, TYV, THV})), drive.loc, new Vector(new double[6]), Q, RM, QF, N, Horizon, threshold, lr, max_lambda, max_vxx);
+        test = new MPCPath();
+        test.setAccuracy(TestMPCParams.threshold);
+        test.setMoveTime(TestMPCParams.Horizon);
+        test.setResolution(((double) TestMPCParams.N)/TestMPCParams.Horizon);
+        test.setParams(DriveConfig.DriveWheels.defaultParams);
+        test.setTarget(TestMPCParams.TX, TestMPCParams.TY, TestMPCParams.TH, TestMPCParams.TV, TestMPCParams.THV);
+        test.setStart(0, 0, 0, 0, 0);
+        try {
+            test.load("Test Path.json");
+        } catch (RuntimeException e) {
+            test.compile(3000);
+            test.save("Test Path.json");
+        }
+
     }
 
     @Override
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motion/Controllers/MPCPath.java
@@ -0,0 +1,242 @@
+package org.firstinspires.ftc.teamcode.Motion.Controllers;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.AAAOpModes.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Utilities.Math.GeneralMatrix;
+import org.firstinspires.ftc.teamcode.Utilities.Math.Matrix;
+import org.firstinspires.ftc.teamcode.Utilities.Math.Vector;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class MPCPath {
+
+    public static class MPCParams {
+        public double QX = 100;
+        public double QY = 100;
+        public double QH = 100;
+        public double QHV = 10;
+        public double QV = 10;
+
+        public double QFX = 20;
+        public double QFY = 20;
+        public double QFH = 20;
+        public double QFV = 10;
+        public double QFHV = 10;
+
+        public double R = 50;
+
+        public double lr = 1.01;
+        public double lambdaMax = 10000;
+    }
+
+    Vector start;
+    double horizonTime;
+    double threshold;
+    double resolution;
+
+
+    MPCParams params;
+
+    ReferenceSignal referenceSignal;
+    Signal sensorSignal;
+
+    ElapsedTime timer;
+
+    MPC controller;
+
+    public void setPath(ReferenceSignal referenceSignal) {
+        this.referenceSignal = referenceSignal;
+    }
+
+    public void setParams(MPCParams params) {
+        this.params = params;
+    }
+
+    public void continueFrom(MPCPath previous) {
+        this.start = previous.referenceSignal.target();
+    }
+
+    public void setTarget(double x, double y, double h, double v, double vh) {
+        this.referenceSignal = new ConstantSignal(new Vector(new double[] {x, y, h, v, vh}));
+    }
+
+    public void setStart(double x, double y, double h, double v, double vh) {
+        this.start = new Vector(new double[] {x, y, h, v, vh});
+    }
+
+    public void setMoveTime(double time) {
+        this.horizonTime = time;
+    }
+
+    public void setAccuracy(double accuracy) {
+        this.threshold = accuracy;
+    }
+
+    public void setResolution(double resolution) {
+        this.resolution = resolution;
+    }
+
+    public void build() {
+        Matrix Q = new GeneralMatrix(5, 5, new double[] {
+                params.QX, 0, 0, 0, 0,
+                0, params.QY, 0, 0, 0,
+                0, 0, params.QH, 0, 0,
+                0, 0, 0, params.QV, 0,
+                0, 0, 0, 0, 0, params.QHV,
+        });
+
+        Matrix QF = new GeneralMatrix(5, 5, new double[] {
+                params.QFX, 0, 0, 0, 0,
+                0, params.QFY, 0, 0, 0,
+                0, 0, params.QFH, 0, 0,
+                0, 0, 0, params.QFV, 0,
+                0, 0, 0, 0, 0, params.QFHV,
+        });
+
+        Matrix R = Matrix.identityMatrix(4).multiplied(params.R);
+
+        controller = new MPC(referenceSignal, sensorSignal, start, Q, R, QF, (int) (resolution*horizonTime), horizonTime, threshold, params.lr,  params.lambdaMax);
+    }
+
+    public void compile(int maxIter) {
+
+        controller.iterate(3000, start);
+
+    }
+
+    public void start() {
+        this.timer = new ElapsedTime();
+    }
+
+    public void save(String name) {
+
+        JsonObject pathData = new JsonObject();
+
+        JsonArray data = new JsonArray();
+
+        for (int i = 0; i < controller.N; i++) {
+            JsonObject step = new JsonObject();
+
+            JsonArray xBytes = new JsonArray();
+            JsonArray uBytes = new JsonArray();
+            JsonArray kBytes = new JsonArray();
+            JsonArray KBytes = new JsonArray();
+
+            for (int control = 0; control < controller.numControls; control++) {
+                uBytes.set(control, new JsonPrimitive(controller.currentControls[i].get(control)));
+                kBytes.set(control, new JsonPrimitive(controller.k[i].get(control)));
+            }
+
+            for (int state = 0; state < controller.dimensions; state++) {
+                xBytes.set(state, new JsonPrimitive(controller.currentTrajectory[i].get(state)));
+                for (int control = 0; control < controller.numControls; control++) {
+                    KBytes.set(control* controller.dimensions + state, new JsonPrimitive(controller.K[i].get(control, state)));
+                }
+            }
+
+            step.add("x", xBytes);
+            step.add("u", uBytes);
+            step.add("k", kBytes);
+            step.add("K", KBytes);
+
+            data.set(i, step);
+        }
+
+        pathData.add("data", data);
+
+        try {
+            FileWriter writer = new FileWriter(name);
+            writer.write(pathData.toString());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void load(String name) {
+
+        Vector[] x = new Vector[controller.N];
+        Vector[] u = new Vector[controller.N];
+        Vector[] k = new Vector[controller.N];
+        Matrix[] K = new Matrix[controller.N];
+
+        try {
+            JsonObject pathData = new JsonParser().parse(new JsonReader(new FileReader(name))).getAsJsonObject();
+
+            JsonArray data = pathData.getAsJsonArray("data");
+
+            for (int i = 0; i < controller.N; i++) {
+                JsonObject step = data.get(i).getAsJsonObject();
+
+                JsonArray xBytes = step.getAsJsonArray("x");
+                JsonArray uBytes = step.getAsJsonArray("u");
+                JsonArray kBytes = step.getAsJsonArray("k");
+                JsonArray KBytes = step.getAsJsonArray("K");
+
+                double[] xdata = new double[controller.dimensions];
+                double[] udata = new double[controller.numControls];
+                double[] kdata = new double[controller.numControls];
+                double[] Kdata = new double[controller.dimensions * controller.numControls];
+                for (int control = 0; control < controller.numControls; control++) {
+                    udata[control] = uBytes.get(control).getAsDouble();
+                    kdata[control] = kBytes.get(control).getAsDouble();
+                }
+
+                for (int state = 0; state < controller.dimensions; state++) {
+                    xdata[i] = xBytes.get(state).getAsDouble();
+                    for (int control = 0; control < controller.numControls; control++) {
+                        Kdata[control*controller.dimensions + state] = KBytes.get(control*controller.dimensions + state).getAsDouble();
+                    }
+                }
+            }
+
+            controller.loadFromArray(x, u, k, K);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Vector getCorrection() {
+
+        if (timer == null) start();
+        double time = timer.time();
+
+        int numDim = this.sensorSignal.getLength();
+        Vector sensorData = Vector.length(numDim * 2);
+
+        for (int i = 0; i < numDim; i++) {
+            sensorData.put(i, sensorSignal.getIntegralVector().get(i));
+            sensorData.put(i+numDim, sensorSignal.getDataVector().get(i));
+        }
+        Vector target = controller.getInterpolatedX(time);
+        BaseOpMode.addData("TX", target.get(0));
+        BaseOpMode.addData("TY", target.get(1));
+        BaseOpMode.addData("TH", target.get(2));
+        BaseOpMode.addData("TVX", target.get(3));
+        BaseOpMode.addData("TVY", target.get(4));
+        BaseOpMode.addData("TVH", target.get(5));
+
+        sensorData.subtract(target);
+        Vector correction = controller.getInterpolatedU(time);
+        return correction.added(controller.getInterpolatedK(time).multiplied(sensorData));
+    }
+
+
+}
