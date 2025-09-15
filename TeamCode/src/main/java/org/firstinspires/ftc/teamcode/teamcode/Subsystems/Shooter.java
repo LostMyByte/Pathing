f85198450b583fc8d51581bf97453063d1fdd597
THEdillster3 <Dylancook555@gmail.com>
2025-09-15 16:50:45 -0400

    Set up the shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Subsystems/Shooter.java
@@ -18,21 +18,43 @@ public class Shooter extends Subsystem{
     AnalogInput turretEncoder;
     double turretAngle;
     PID shooterPDF;
+    PID turretPDL;
     double targetShooterRPM;
-    Drivetrain drivetrain;
+    double xPosition;
+    double yPosition;
+    double xVelocity;
+    double yVelocity;
+    double heading;
+    ShooterStates shooterStates;
+    double ticksPerRotation;
+    double turretStartAngle;
+    double turretTargetAngle;
 
 
-    public Shooter(HardwareMap hardwareMap){
+
+    public Shooter(HardwareMap hardwareMap, double turretStartAngle){
         shooter1 = new Motor(Hardware.shooter1);
         shooter2 = new Motor(Hardware.shooter2);
         hood = new Servos.Hood();
         turret = new Motor(Hardware.turret);
         turretEncoder = hardwareMap.get(AnalogInput.class, "turretEncoder");
         shooterPDF = new PID(0,0,0);
+        turretPDL = new PID(0,0,0);
 
-        drivetrain = new Drivetrain(hardwareMap,0);
-
+        this.turretStartAngle = turretStartAngle;
     }
+
+    public void work(){
+        switch (shooterStates){
+            case ACTIVE:
+                aim();
+                updateShooter();
+            case NOTACTIVE:
+                aim();
+            case OBELISK:
+
+        }
+    };
 
 
 
@@ -48,13 +70,53 @@ public class Shooter extends Subsystem{
         shooter2.setPower(correction);
     }
 
+    public void updateTurret(){
+        double angle = turret.encoder.getPosition();
+        //convert this to radians and wrap the angle
+        angle = angle * (2*Math.PI/ticksPerRotation);
+
+        while (angle > Math.PI){
+            angle -= 2*Math.PI;
+        }
+        while (angle < -Math.PI){
+            angle += 2*Math.PI;
+        }
+
+        turretPDL.setConstants(0,0,0);
+        turretPDL.getCorrectionHeading(angle,turretTargetAngle);
+
+        turret.setPower(turretPDL.getCorrectionHeading(angle,turretTargetAngle));
+    }
+
     public double setTargetBallSpeed(double speed){
         //make a regression comparing ball exit velocity to shooter RPM
         targetShooterRPM = speed;
         return targetShooterRPM;
     }
 
+    public void aim(){
+        hood.setPositionInterpolated(getHoodAngle());
 
+
+        turretTargetAngle = Math.asin(xVelocity/getBallSpeed()) - heading;
+
+        while (turretTargetAngle > Math.PI){
+            turretTargetAngle -= 2*Math.PI;
+        }
+        while (turretTargetAngle < -Math.PI){
+            turretTargetAngle += 2*Math.PI;
+        }
+        updateTurret();
+    }
+
+    public double getBallSpeed(){
+        //invert the regression comparing ball exit velocity to shooter RPM
+        return 1;
+    }
+
+    public double getHoodAngle(){
+        return 1;
+    }
 
     @Override
     public void updateSensors() {
@@ -63,7 +125,10 @@ public class Shooter extends Subsystem{
 
     @Override
     public void update() {
-        updateShooter();
+        work();
     }
 
+    public enum ShooterStates{
+        ACTIVE, NOTACTIVE, OBELISK;
+    }
 }
