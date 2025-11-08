4d3ef445a3039ddd8349a1b8cfc93215587d03ba
Kieran <lostmybyte@gmail.com>
2025-11-08 14:42:56 -0500

    Did inheritance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Subsystems/TeliOpDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Subsystems/TeliOpDrivetrain.java
@@ -27,7 +27,7 @@ import org.firstinspires.ftc.teamcode.teamcode.Utilities.Math.Vector;
 import org.firstinspires.ftc.teamcode.teamcode.Utilities.Vision.BallChaser;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
-public class TeliOpDrivetrain extends Subsystem {
+public class TeliOpDrivetrain extends TankDriveTrain {
     @Config
     public static class DrivetrainDash {
         public static double rateOfChangeThreshold = 120;
@@ -53,7 +53,7 @@ public static double
         public static double visionDriveDeadzone = 5;
         public static double visionDistanceTarget = 138; //pixels bc goofy
     }
-    MecanumDrive driveWheels;
+
 
     PID pid;
 
@@ -68,8 +68,6 @@ public static double
 
     PID visionTurnPID;
     PID visionDrivePID;
-
-    TankDriveTrain driveWheels2;
 
     Vector theFuckassTankDriveVector = new Vector(0,0,0);
 
@@ -88,7 +86,6 @@ public static double
     public TeliOpDrivetrain(HardwareMap hardware, double heading) {
 
         gamepad1 = new Gamepad();
-        driveWheels2 = new TankDriveTrain();
       //  driveWheels = new MecanumDrive();
         visionTurnPID = new PID(DrivetrainDash.kPturn, DrivetrainDash.kIturn, kDturn);
         visionDrivePID = new PID(DrivetrainDash.kPdrive, DrivetrainDash.kIdrive, DrivetrainDash.kDdrive);
@@ -242,7 +239,7 @@ public static double
         multTelemetry.addData("angle in radians", angleRad);
         multTelemetry.addData("scuffed distance", scuffedDistance(BallChaser.getWidth(trueIfGreen), visionDistanceTarget, trueIfGreen));
         multTelemetry.addData("aspect ratio", aspectRatio);
-        driveWheels2.drive(drive,turn);
+        drive(drive,turn);
       //  driveWheels.veryDirectDrive(drive +strafe -turn,drive -strafe +turn,drive -strafe -turn,drive +strafe +turn);
 //if check aspect ratio of detection, then multiply target width by ratio
 /* fl.setPower((drive -strafe +turn));
